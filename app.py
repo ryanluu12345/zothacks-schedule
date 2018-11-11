1e1fc5c744f3adf0086a60f489c18c3c26bb9f4c
Ryan Luu <ryanluu12345@gmail.com>
2018-11-11 11:42:31 -0800

    Refactored project to be compliant with Flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route("/",methods = ["GET"])
+def main():
+    with open("one_day_schedule.json") as schedule:
+        events = json.load(schedule)
+    return render_template("index.html", events = events)
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
